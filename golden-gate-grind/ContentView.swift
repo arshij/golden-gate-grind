43224c3a516230f527261ad90779b6cb5387e064
Arshi Jujara <me@arshi.com>
2020-08-06 19:19:56 -0700

    Initial Commit

---
+++ golden-gate-grind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  golden-gate-grind
+//
+//  Created by Arshi Jujara on 8/6/20.
+//  Copyright © 2020 Arshi Jujara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
